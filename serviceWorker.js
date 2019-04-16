0aee50f16063073472fc592ba256ced88d20b33a
algorythm <anders@wiberg.tech>
2019-04-16 21:47:51 +0200

    Optimizing SEO

--- serviceWorker.js
+++ serviceWorker.js
@@ -13,10 +13,26 @@ self.addEventListener('install', async event => {
 
 self.addEventListener('fetch', event => {
     const request = event.request
-    event.respondWith(cacheFirst(request))
+    if (URL.origin === location.origin) {
+        event.respondWith(cacheFirst(request))
+    } else {
+        event.respondWith(networkFirst(request))
+    }
 })
 
 async function cacheFirst(request) {
     const cachedResponse = await caches.match(request)
     return cachedResponse || fetch(request)
+}
+
+async function networkFirst(request) {
+    const cache = await caches.open('xfm-static')
+
+    try {
+        const response = await fetch(request)
+        cache.put(request, response.clone())
+        return response
+    } catch(error) {
+        return await cache.match(request)
+    }
 }
